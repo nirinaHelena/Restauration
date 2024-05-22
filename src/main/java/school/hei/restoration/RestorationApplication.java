a4029e26e3a4cbd8705f2ccd884762418de6af9c
nirinaHelena <hei.nirina.7@gmail.com>
2024-05-22 08:31:01 +0300

    chore: initialize mvn project

---
+++ src/main/java/school/hei/restoration/RestorationApplication.java
@@ -0,0 +1,13 @@
+package school.hei.restoration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestorationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestorationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/school/hei/restoration/RestorationApplicationTests.java
@@ -0,0 +1,13 @@
+package school.hei.restoration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestorationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
