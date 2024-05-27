931a10077aaa062a88c85214736ee39a0fd53c62
nirinaHelena <hei.nirina.7@gmail.com>
2024-05-27 08:33:17 +0300

    feat: make stock movement details accessible by api

--- src/main/java/school/hei/restoration/endpoint/rest/StockController.java
+++ src/main/java/school/hei/restoration/endpoint/rest/StockController.java
@@ -3,12 +3,14 @@ package school.hei.restoration.endpoint.rest;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import school.hei.restoration.Service.StockService;
+import school.hei.restoration.repository.model.Movement;
+import school.hei.restoration.repository.model.Restaurant;
 import school.hei.restoration.repository.model.Stock;
+
+import java.time.Instant;
+import java.util.List;
 
 
 @AllArgsConstructor
@@ -21,5 +23,14 @@ public class StockController {
     public ResponseEntity<Stock> supply(@RequestBody Stock stock){
         Stock savedStock = stockService.supply(stock);
         return new ResponseEntity<>(savedStock, HttpStatus.CREATED);
+    }
+    @GetMapping("/get-stock-movement-details-at-date")
+    public ResponseEntity<List<Movement>> getStockDetailsMovement(@RequestBody Restaurant restaurant,
+                                                                  @RequestParam String beginString,
+                                                                  @RequestParam String endString){
+        Instant begin = Instant.parse(beginString);
+        Instant end = Instant.parse(endString);
+        List<Movement> movements = stockService.getStockDetailsMovementAtDate(restaurant, begin, end);
+        return new ResponseEntity<>(movements, HttpStatus.OK);
     }
 }
