6f4596bbe1047e94cbb4a2790d8e01cdb25d78fb
nirinaHelena <hei.nirina.7@gmail.com>
2024-05-27 08:43:55 +0300

    feat: make get menu's ingredient accessible by api

--- src/main/java/school/hei/restoration/endpoint/rest/MenuController.java
+++ src/main/java/school/hei/restoration/endpoint/rest/MenuController.java
@@ -3,22 +3,33 @@ package school.hei.restoration.endpoint.rest;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import school.hei.restoration.Service.MenuService;
+import school.hei.restoration.repository.MenuRepo;
+import school.hei.restoration.repository.model.Ingredient;
 import school.hei.restoration.repository.model.Menu;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("/menu")
 public class MenuController {
     private final MenuService menuService;
+    private final MenuRepo menuRepo;
 
     @PostMapping
     public ResponseEntity<Boolean> save(@RequestBody Menu menu){
         menuService.save(menu);
         return new ResponseEntity<>(true,HttpStatus.CREATED);
+    }
+    @GetMapping("/get-menu-ingredient/{idMenu}")
+    public ResponseEntity<List<Ingredient>> getMenuIngredient(@PathVariable int idMenu){
+        List<Ingredient> ingredients = menuService.getAllMenuIngredient(menuRepo.getMenuById(idMenu));
+        if (ingredients != null) {
+            return new ResponseEntity<>(ingredients, HttpStatus.OK);
+        }else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
