21155ada1b0a55e6c60acc576b2bc47dd53b662f
Ny Hasina VAGNO <hei.nyhasina@gmail.com>
2024-06-12 16:00:45 +0300

    chore: refactor ingredient service test

--- src/test/java/school/hei/restoration/service/IngredientServiceTest.java
+++ src/test/java/school/hei/restoration/service/IngredientServiceTest.java
@@ -19,6 +19,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
@@ -37,51 +38,60 @@ class IngredientServiceTest {
     @BeforeEach
     public void setUp(){
         MockitoAnnotations.openMocks(this);
+
+        when(movementRepo.getIngredientMostUsed(any(Integer.class), any(Instant.class), any(Instant.class))).thenReturn(List.of(bread(), sausage()));
+        when(ingredientTemplateRepo.getById(1)).thenReturn(breadTemplate());
+        when(ingredientTemplateRepo.getById(2)).thenReturn(sausageTemplate());when(ingredientRepo.menuWhereIngredientIsMostUsed(eq(breadTemplate()))).thenReturn(burger());
+        when(ingredientRepo.menuWhereIngredientIsMostUsed(eq(sausageTemplate()))).thenReturn(hotDog());
+
     }
 
+    public IngredientMostUsed bread(){
+        return new IngredientMostUsed(1, 100);
+    }
+
+    public IngredientMostUsed sausage(){
+        return new IngredientMostUsed(2, 200);
+    }
+
+    public IngredientTemplate breadTemplate(){
+        return new IngredientTemplate(1, "bread", 500,
+                new Unity(1, "piece"));
+    }
+
+    public IngredientTemplate sausageTemplate(){
+        return new IngredientTemplate(2, "sausage", 500,
+                new Unity(2, "kg"));
+    }
+
+    public Menu burger(){
+        return new Menu(1, "burger");
+    }
+
+    public Menu hotDog(){
+        return new Menu(2, "hot dog");
+    }
+
+
     @Test
-    void ingredientMostUsedByMenus() {
+    void read_most_used_ingredients_by_menu_id_ok(){
         int limit = 3;
         Instant begin = Instant.parse("2023-01-01T00:00:00Z");
         Instant end = Instant.parse("2023-12-31T23:59:59Z");
 
-        IngredientMostUsed bread = new IngredientMostUsed(1, 100);
-        IngredientMostUsed sausage = new IngredientMostUsed(2, 200);
-        List<IngredientMostUsed> ingredientMostUses = Arrays.asList(bread, sausage);
+        List<IngredientMostUsedByMenu> actual = ingredientService.ingredientMostUsedByMenus(limit, begin, end);
+        List<Menu> menus = actual.stream().map(IngredientMostUsedByMenu::menuWhichMostUsedIngredient)
+                        .toList();
 
-        Unity piece = new Unity(1, "piece");
-        IngredientTemplate breadTemplate = new IngredientTemplate(1, "bread", 500, piece);
-
-        Unity kg = new Unity(2, "kg");
-        IngredientTemplate sausageTemplate = new IngredientTemplate(2, "sausage", 500, kg);
-
-        when(movementRepo.getIngredientMostUsed(eq(limit), eq(begin), eq(end))).thenReturn(ingredientMostUses);
-        when(ingredientTemplateRepo.getById(1)).thenReturn(breadTemplate);
-        when(ingredientTemplateRepo.getById(2)).thenReturn(sausageTemplate);
-        Menu burger = new Menu(1, "burger");
-        when(ingredientRepo.menuWhereIngredientIsMostUsed(eq(breadTemplate))).thenReturn(burger);
-        Menu hotDog = new Menu(2, "hot dog");
-        when(ingredientRepo.menuWhereIngredientIsMostUsed(eq(sausageTemplate))).thenReturn(hotDog);
-
-        List<IngredientMostUsedByMenu> ingredientMostUsedByMenus = ingredientService.ingredientMostUsedByMenus(limit, begin, end);
-
-        assertEquals(2, ingredientMostUsedByMenus.size());
-
-        IngredientMostUsedByMenu result1 = ingredientMostUsedByMenus.get(0);
-        assertEquals(1, result1.idIngredientTemplate());
-        assertEquals("bread", result1.nameIngredient());
-        assertEquals(burger, result1.menuWhichMostUsedIngredient());
-
-        IngredientMostUsedByMenu result2 = ingredientMostUsedByMenus.get(1);
-        assertEquals(2, result2.idIngredientTemplate());
-        assertEquals("sausage", result2.nameIngredient());
-        assertEquals(hotDog, result2.menuWhichMostUsedIngredient());
+        assertEquals(2, actual.size());
+        assertTrue(menus.contains(burger()));
+        assertTrue(menus.contains(hotDog()));
 
         verify(movementRepo).getIngredientMostUsed(eq(limit), eq(begin), eq(end));
         verify(ingredientTemplateRepo).getById(1);
         verify(ingredientTemplateRepo).getById(2);
-        verify(ingredientRepo).menuWhereIngredientIsMostUsed(eq(breadTemplate));
-        verify(ingredientRepo).menuWhereIngredientIsMostUsed(eq(sausageTemplate));
-
+        verify(ingredientRepo).menuWhereIngredientIsMostUsed(eq(breadTemplate()));
+        verify(ingredientRepo).menuWhereIngredientIsMostUsed(eq(sausageTemplate()));
     }
+
 }
